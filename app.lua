8b0c20611d35890c2ac037d23344aae71579b63e
spiderforrest <spider@spood.org>
2024-06-19 14:13:24 -0700

    finalize

--- app.lua
+++ app.lua
@@ -7,7 +7,7 @@ app.layout = require "views.layout"
 
 -- directly render each etlua page
 local render = function () return { render = true } end
-app:get("about", "/about", render)
+-- app:get("about", "/about", render)
 app:get("contact", "/contact", render)
 app:get("home", "/", render)
 
